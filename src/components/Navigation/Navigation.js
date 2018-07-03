1c65d3cbf37b35cb08e871ad3b4b8b8449baf7fa
Mr Booi <ayabongabooi2@gmail.com>
2018-07-03 15:42:53 +0200

    Add component for Navigation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+const Navigation = () => {
+
+    return (
+        <nav>
+         <p>Sign out</p>
+        </nav>
+
+    )
+
+
+};
+
+export default Navigation;
