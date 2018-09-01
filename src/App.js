fcedfc998592cfee13abfb444ac30eab71a0ef3b
Mr Booi <ayabongabooi2@gmail.com>
2018-09-01 18:14:18 +0200

    done this app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Register from './components/Register/Register';
 import SignIn from './components/SignIn/SignIn';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
 
@@ -45,20 +45,7 @@ const initialState= {
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      'input': '',
-      'imageURL': '',
-      'box': {},
-      route: 'SignIn',
-      isSignedIn: false,
-      user:{
-        id: '',
-        name: '',
-        email:'' ,
-      entries: 0,
-      joined: ''
-      }
-    }
+    this.state = initialState;
   }
   loadUser = (user)=>{
     this.setState({user:{
@@ -71,11 +58,14 @@ class App extends Component {
   }
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
+
+    console.log("found",clarifaiFace.left_col * width);
+    
     return {
-      leftcol: clarifaiFace.left_col * width,
+      leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
@@ -98,10 +88,11 @@ class App extends Component {
       headers:{'Content-Type':'application/json'},
        body : JSON.stringify({
          input:this.state.input,
+         id :this.state.user.id
     })
   })   .then(response => response.json())
       .then(response => {
-        if(response){
+        if (response){
           fetch('https://vast-wildwood-61643.herokuapp.com/image',{
             method:'put',
         headers:{'Content-Type':'application/json'},
@@ -111,10 +102,10 @@ class App extends Component {
         })
         .then(response =>response.json())
         .then(count => {
-      
          this.setState(Object.assign(this.state.user,{ entries:count }))
         })
         }
+
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
       
@@ -129,7 +120,7 @@ class App extends Component {
     }
      this.setState({route: route });
   }
-
+ 
 
   render() {
    const {isSignedIn, imageURL , route, box , user} = this.state;
@@ -151,7 +142,7 @@ class App extends Component {
             <FaceRecognition box={box} imageURL={imageURL} />
           </div>
           : (
-            this.state.route ==='SignIn'
+            route ==='SignIn'
              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
              : <Register  loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
